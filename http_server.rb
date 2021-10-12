198204b2a1a5685c73d1c21816139e8aca4bff69
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-10-12 23:35:45 +0300

    2 ex add hdd_type

--- http_server.rb
+++ http_server.rb
@@ -8,7 +8,13 @@ set :port, 5678
 get '/vm' do
     cpu_price = PriceLoader.get_type_price('cpu') * params['cpu'].to_i
     ram_price = PriceLoader.get_type_price('ram') * params['ram'].to_i
-    hdd_price = PriceLoader.get_type_price(params['hdd_type']) * params['hdd_capacity'].to_i
+    hdd_price = 0
+
+    params['hdd_type'].each do |hdd|
+        hdd = hdd.split('-')
+        hdd_price += PriceLoader.get_type_price(hdd[0]) * hdd[1].to_i
+    end
+
     vm_price = cpu_price + ram_price + hdd_price
     vm_price.to_s
 end
