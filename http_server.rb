eac51b7062ab5197dc4d79366fb47890df0833ad
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-10-17 16:20:12 +0300

    reduce code

--- http_server.rb
+++ http_server.rb
@@ -1,19 +1,14 @@
 require 'sinatra'
 
-require './loader/price_loader.rb'
+require './price_calc.rb'
 
 set :bind, '0.0.0.0'
 set :port, 5678
 
 get '/vm' do
-    cpu_price = PriceLoader.get_type_price('cpu') * params['cpu'].to_i
-    ram_price = PriceLoader.get_type_price('ram') * params['ram'].to_i
-    hdd_price = 0
-
-    params['hdd_type'].each do |hdd|
-        hdd = hdd.split('-')
-        hdd_price += PriceLoader.get_type_price(hdd[0]) * hdd[1].to_i
-    end
+    cpu_price = PriceCalc.price_cpu(params)
+    ram_price = PriceCalc.price_ram(params)
+    hdd_price = PriceCalc.price_hdd(params)
 
     vm_price = cpu_price + ram_price + hdd_price
     vm_price.to_s
