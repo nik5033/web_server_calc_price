4d68c1d36bbeba2defbee8d92c3149e732d871e3
Nikita Gudkov <gudkov.nikita@bk.ru>
2021-10-27 20:17:44 +0300

    add method for calc add disks cost

--- price_calc.rb
+++ price_calc.rb
@@ -12,4 +12,18 @@ class PriceCalc
     def self.price_ram(params)
         PriceLoader.get_type_price('ram') * params['ram'].to_i
     end
+
+    def self.price_add_hdd(params)
+        hdd_price = 0
+
+        if params['add_hdd_type'] && params['add_hdd_capacity']
+            add_disk = Hash[params['add_hdd_type'].zip(params['add_hdd_capacity'])]
+            
+            add_disk.each do |hdd_type, hdd_capacity|
+                hdd_price += PriceLoader.get_type_price(hdd_type) * hdd_capacity.to_i
+            end
+        end
+
+        hdd_price
+    end
 end
